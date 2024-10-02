d941e2ee03e27403929227b26478c7d1fdebfb5f
Valentina Toni <vale.toni93@gmail.com>
2024-10-02 17:33:07 +0200

    DONE: added button component

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,17 @@ export default {
         primary: {
           DEFAULT: 'hsl(252, 89%, 63%)',
           hover: 'hsl(252, 89%, 73%)',
-          accent: 'hsl(240, 14%, 97%)',
-          text: 'hsl(0, 0%, 1%)',
-          secondary: {
-            DEFAULT: 'hsl(0, 0%, 11%)',
-            hover: 'hsl(0, 0%, 21%)',
-          },
-          background: 'hsl(240, 5%, 95%)',
         },
-      }
+        accent: 'hsl(240, 14%, 97%)',
+        text: 'hsl(0, 0%, 1%)',
+        secondary: {
+          DEFAULT: 'hsl(0, 0%, 11%)',
+          hover: 'hsl(0, 0%, 21%)',
+        },
+        background: 'hsl(240, 5%, 95%)',
+      },
     }
+
   },
   plugins: [],
 }
