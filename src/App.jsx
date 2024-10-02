7df4321d1388c8c54ce84b7d5dcb23afd60f9eec
Valentina Toni <vale.toni93@gmail.com>
2024-10-02 19:35:01 +0200

    DONE: added general layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,73 @@
 
-import './App.css'
 import Button from './components/Button'
 import Chip from './components/Chip'
+import CustomShape from './components/CustomShape'
 import Logo from './components/Logo'
 import MainImage from './components/MainImage'
 
 function App() {
 
+  const chipList = [
+    {
+      id: 1,
+      text: 'Social',
+    },
+    {
+      id: 2,
+      text: 'Educació'
+    },
+    {
+      id: 3,
+      text: 'Cuinar'
+    },
+    {
+      id: 4,
+      text: 'Relaxació'
+    },
+    {
+      id: 5,
+      text: 'Treball Intens'
+    },
+    {
+      id: 6,
+      text: 'Caritat'
+
+    },
+    {
+      id: 7,
+      text: 'Recreatiu'
+    }
+  ]
+
   return (
-    <>
-      <Logo />
-      <MainImage />
-      <Button type="button" text="Clicca qui" handleClick={() => alert('Ciao!')} />
-      <Chip text="Social"/>
-    </>
+    <div className="min-h-screen">
+      <header className="p-5">
+        <Logo />
+      </header>
+      <main className='p-5 grid grid-cols-1 lg:grid-cols-2 items-center justify-center h-full'>
+        <div className="flex flex-col items-center justify-center border-r-2 p-6 border-transparent border-dashed lg:border-black">
+          <ul className="flex flex-wrap gap-2 items-center justify-center">
+            {
+              chipList.map(chip => <li key={chip.id}>
+                <Chip text={chip.text} handleClick={() => alert(chip.text)} />
+              </li>)
+            }
+          </ul>
+          <div className='relative flex flex-col items-center justify-center gap-5'>
+            <MainImage />
+            <CustomShape />
+            <h1 className="text-3xl font-bold uppercase text-center max-w-[300px]">trova alguna cosa a fer</h1>
+            <Button type="button" text="Generar" handleClick={() => alert('Ciao!')} />
+          </div>
+
+        </div>
+        <div className="flex flex-col items-center justify-center p-6 mt-10 lg:mt-0">
+          <h2 className="text-3xl font-semibold my-3">Activitat:</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </main>
+
+    </div>
   )
 }
 
---
+++ src/components/CustomShape.jsx
@@ -0,0 +1,9 @@
+const CustomShape = () => {
+    return (
+        <div 
+            className="w-[70vw] h-[40vh] bg-accent absolute -z-10 rounded-3xl max-w-[300px] max-h-[250px] top-[245px] ">
+        
+        </div>
+    )
+}
+export default CustomShape
